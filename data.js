08dc9f7459a5fa9804ae61b06f17efd768a41785
unknown <lpsk5713@gmail.com>
2021-10-22 02:30:31 +0530

    Added Data in JSON format in data.js file

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        "url": "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4",
+        "channel": "redLady",
+        "description": 'Swimming Jazz',
+        "song": 'ABCD Song',
+        "likes": "123",
+        "messages": "456",
+        "shares": "345"
+    },
+    {
+        "url": "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4",
+        "channel": "pinkLady",
+        "description": 'Swimming Jazz',
+        "song": 'ABCD Song',
+        "likes": "1223",
+        "messages": "4456",
+        "shares": "310"
+    }
+
+]
+
+export default data;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express"
 import mongoose from "mongoose"
+import data from "./data";
 
+
+// App config
 const app = express();
 const port = 9000;
 
+// Middle wares
+
+// DB Config
+
+// API endpoints
 app.get('/', (req, res) => res.status(200).send("Hello World"));
 
 app.listen(port, () => console.log(`Listening on Local host : ${port}`))
